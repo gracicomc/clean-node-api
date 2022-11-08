bcb1bdc7513b513a70834c6fea82f3ee041200e1
Graci <graciparentte@gmail.com>
2022-11-08 17:50:03 -0300

    add jestconfig

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jest-environment-node'
+
+  // The glob patterns Jest uses to detect test files
+  // testMatch: [
+  //   "**/__tests__/**/*.[jt]s?(x)",
+  //   "**/?(*.)+(spec|test).[tj]s?(x)"
+  // ],
+
+}
