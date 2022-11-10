c0aab7e7dcbf16285e0f5516d4e306408e4f74f0
Graci <graciparentte@gmail.com>
2022-11-10 15:52:45 -0300

    test: ensure LoginRouter call AuthUseCase with correct params

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,11 +2,25 @@ const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
 const makeSut = () => {
-  return new LoginRouter()
-}
+  class AuthUseCaseSpy {
+    auth (email, password) {
+      this.email = email
+      this.password = password
+    }
+  }
+
+  const authUseCaseSpy = new AuthUseCaseSpy()
+  const sut = new LoginRouter(authUseCaseSpy) // dependencie injection
+
+  return {
+    sut,
+    authUseCaseSpy
+  }
+} // this is a design patern called factory
+
 describe('Login Router', () => {
   it('should return 400 if email is not provided', () => {
-    const sut = makeSut() // sut: system under test
+    const { sut } = makeSut() // sut: system under test
 
     const httpRequest = {
       body: {
@@ -20,7 +34,7 @@ describe('Login Router', () => {
   })
 
   it('should return 400 if password is not provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -34,7 +48,7 @@ describe('Login Router', () => {
   })
 
   it('should return 500 if httpRequest is not provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpResponse = sut.route()
 
@@ -42,7 +56,7 @@ describe('Login Router', () => {
   })
 
   it('should return 500 if httpRequest has no body', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
@@ -51,11 +65,17 @@ describe('Login Router', () => {
   })
 
   it('should call AuthUseCase with correct params', () => {
-    const sut = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
 
-    const httpRequest = {}
-    const httpResponse = sut.route(httpRequest)
+    const httpRequest = {
+      body: {
+        email: 'email@email.com',
+        password: 'pass1234'
+      }
+    }
+    sut.route(httpRequest)
 
-    expect(httpResponse.statusCode).toBe(500)
+    expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
+    expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
   })
 })
