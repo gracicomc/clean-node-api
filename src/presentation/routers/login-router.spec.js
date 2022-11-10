4cdfbc4f28d4219d4375f1e2f253a908a87cfda0
Graci <graciparentte@gmail.com>
2022-11-10 11:26:19 -0300

    test: ensure LoginRouter returns 500 if httpRequest has no body or its not provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,10 @@
 class LoginRouter {
   route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
     const { email, password } = httpRequest
     if (!email || !password) {
       return {
@@ -12,23 +17,44 @@ class LoginRouter {
 describe('Login Router', () => {
   it('should return 400 if email is not provided', () => {
     const sut = new LoginRouter() // sut: system under test
+
     const httpRequest = {
       body: {
         password: 'pass1234'
       }
     }
     const httpResponse = sut.route(httpRequest)
+
     expect(httpResponse.statusCode).toBe(400)
   })
 
   it('should return 400 if password is not provided', () => {
     const sut = new LoginRouter()
+
     const httpRequest = {
       body: {
         email: 'email@email.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
+
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('should return 500 if httpRequest is not provided', () => {
+    const sut = new LoginRouter()
+
+    const httpResponse = sut.route()
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  it('should return 500 if httpRequest has no body', () => {
+    const sut = new LoginRouter()
+
+    const httpRequest = {}
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
