0dd8bdcce855641ee7343a52e302f8da2eb85ed1
Graci <graciparentte@gmail.com>
2022-11-10 17:18:36 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -83,7 +83,7 @@ describe('Login Router', () => {
   })
 
   it('should return 200 when valid credentials are provided', () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
 
     const httpRequest = {
       body: {
@@ -94,6 +94,7 @@ describe('Login Router', () => {
     const httpResponse = sut.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
   })
 
   it('should return 401 when invalid credentials are provided', () => {
